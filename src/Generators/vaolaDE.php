2a96a3df5828cfe9276397589e8218326ce4a607
joe1111 <jonas.schaetzle@gmail.com>
2017-11-08 13:16:30 +0100

    remove max

--- src/Generators/vaolaDE.php
+++ src/Generators/vaolaDE.php
@@ -252,8 +252,7 @@ class vaolaDE extends CSVGenerator
             if($sportart == "" || $sportart == 0){
                 $sportart = "Radsport";
             }
-            
-            $allprices = implode($this->vaolaHelper->getAllPrices($item));
+            $allprices = "TODO: ".implode(",", $this->vaolaHelper->getAllPrices($item));
             $vk = number_format($this->vaolaHelper->getPrice($item), 2, '.', '');
             $uvp = number_format($this->vaolaHelper->getRecommendedRetailPrice($item, $settings), 2, '.', '');
             if($uvp == "0.00"){
@@ -372,7 +371,7 @@ class vaolaDE extends CSVGenerator
                 $size = "Unisize";
             }
             
-            $allprices = implode($this->vaolaHelper->getAllPrices($item));
+            $allprices = "TODO: ".implode(",", $this->vaolaHelper->getAllPrices($item));
             
         
         $stockList = $this->getStockList($item);
@@ -477,7 +476,7 @@ class vaolaDE extends CSVGenerator
                 $size = "Unisize";
             }
            
-            $allprices = implode($this->vaolaHelper->getAllPrices($item));
+            $allprices = "TODO: ".implode(",", $this->vaolaHelper->getAllPrices($item));
             
         $stockList = $this->getStockList($item);
         
